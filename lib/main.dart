8fbccebc2468847a1f0abcd37bfd0681e71aec97
ritwik_ghosh153 <ritwik.ghosh1010@gmail.com>
2020-04-07 23:28:24 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp((CRUDapp()));
+}
+
+class CRUDapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
